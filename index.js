53053efebd0bf25f9f40d1d200b9582347f9a1f5
jmjubaer <jmjubaer3927@gmail.com>
2023-06-10 14:06:18 +0600

    make selected class api

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-
+const jwt = require('jsonwebtoken')
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "Unauthorize access"})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded) =>{
+    if(err){
+      return res.status(403).send({ message: "Forbidden access"})
+    }else{
+      req.decoded = decoded;
+      next();
+    }
+  })
+}
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.VITE_USER}:${process.env.VITE_PASS}@cluster0.wxf5f5n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +43,12 @@ async function run() {
     const userCollection = client.db('Music-hub').collection('user');
     const enrolledCollection = client.db('Music-hub').collection('enrolled');
 
+    //JWT related API
+    app.post('/jwt',async(req,res) =>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.VITE_TOKEN,{expiresIn: '1h'})
+      res.send({token});
+    })
 
 
     // Class related api =================================================
@@ -61,10 +82,33 @@ async function run() {
     })
 
     // Enrolled related api
+    app.get('/selectedclass',verifyJWT, async(req, res) => {
+      const query = {status: "pending"};
+      const result = await enrolledCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/enrolledclass',verifyJWT, async(req, res) => {
+      const query = {status: "paid"};
+      const result = await enrolledCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/enrolled',async(req, res) => {
-      
+      const classInfo = req.body;
+      const query = {
+        productId: classInfo.productId,
+        email: classInfo.email
+      }
+      const isSelected = await enrolledCollection.find(query).toArray();
+      if(!isSelected.length > 0) {
+        const result = await enrolledCollection.insertOne(classInfo);
+        res.send(result);
+      }else{
+        res.send({selected: true});
+      }
     })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
